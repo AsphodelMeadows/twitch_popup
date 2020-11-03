cab1e81a5ff29009f96f51c3532240100d97f1b2
Moja <marine.zouaoui@hotmail.com>
2020-11-03 22:21:06 +0100

    Timer is fixed

--- src/handlers.js
+++ src/handlers.js
@@ -10,13 +10,11 @@ actionHandlers['!prout'] = {
         const formattedText = popup.formatEmotes(textContent, context.emotes, true).substr(commande);
         popup.showText(formattedText);
 
-        //Auto erase the message after 5min (30000)
-        var x = setTimeout(myTimer, 300000);
-        function myTimer() {
-        $("#popupbox").animate({ width: 0}, 700);
-        $("#popupbox").animate({ border: 0}, 700);
-        $("#popuptext").animate({ "opacity": 0, "margin-left": "50px" }, 900);
-        }
-        clearTimeout(x);
+        //Auto erase the message after 5min (300000)
+        setTimeout(function myTimer() {
+            $("#popupbox").animate({ width: 0}, 700);
+            $("#popupbox").animate({ border: 0}, 700);
+            $("#popuptext").animate({ "opacity": 0, "margin-left": "50px" }, 900);
+        }, 300000);
     }
 };
